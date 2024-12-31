f21ea97c6e9f8532aa9830fd98d0274b91ef2df1
Topleft <sworkman1562@gmail.com>
2024-12-30 19:20:25 -0600

    kk

--- FRONTEND/MAINDEX/src/config/constants/chains.ts
+++ FRONTEND/MAINDEX/src/config/constants/chains.ts
@@ -10,10 +10,10 @@ export const NeonDevNet = defineChain({
     symbol: 'TNEON',
   },
   rpcUrls: {
-     public: { http: ['https://neon-evm-devnet.rpc.thirdweb.com/'] },
-    default: { http: ['https://neon-evm-devnet.rpc.thirdweb.com/'] },
-    // public: { http: ['https://devnet.neonevm.org/'] },
-    // default: { http: ['https://devnet.neonevm.org/'] },
+    // public: { http: ['https://neon-evm-devnet.rpc.thirdweb.com/'] },
+    // default: { http: ['https://neon-evm-devnet.rpc.thirdweb.com/'] },
+    public: { http: ['https://devnet.neonevm.org/'] },
+    default: { http: ['https://devnet.neonevm.org/'] },
   },
   blockExplorers: {
     etherscan: { name: 'Neon Scan', url: 'https://devnet.neonscan.org/' },
